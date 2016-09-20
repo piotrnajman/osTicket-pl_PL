b12fab17057eef06f64f5a02f86d13eba8844b58
piotrnajman <pnajman@us.edu.pl>
2016-09-20 14:32:02 +0200

    Initial commit

---
+++ MANIFEST.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'Build-Date' => 'Mon, 28 Mar 16 20:25:53 +0000',
+  'Build-Version' => 'v1.9.13-1-g54a70b5',
+  'Language' => 'pl_PL',
+  'Id' => 'lang:pl',
+  'Last-Revision' => '2016-03-28 14:48-0400',
+  'Version' => 145919,
+);
